7aa6001ed9ac00becfd33deb05d244e726ee4ebe
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-10-23 17:05:01 -0400

    init

---
+++ Phone/Phone/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Phone.Models;
+
+namespace Phone.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
