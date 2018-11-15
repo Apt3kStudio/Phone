b98444d4dd114d342a3c9392164c14addfa309fe
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-11-14 22:30:52 -0500

    #11 Added new person class and added two properties and one functions that returns the full name

---
+++ Phone/Phone/Models/Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone.Models
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string FullName()
+        {
+            return FirstName + " " + LastName;
+        }
+    }
+}
