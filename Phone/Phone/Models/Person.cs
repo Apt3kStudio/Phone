e9487174d0fbbee1900b4dc0dbabd38df83984f3
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-11-14 22:32:23 -0500

    Change lastname

--- Phone/Phone/Models/Person.cs
+++ Phone/Phone/Models/Person.cs
@@ -7,7 +7,7 @@ namespace Phone.Models
     class Person
     {
         public string FirstName { get; set; }
-        public string LastName { get; set; }
+        public string LastName1 { get; set; }
 
         public string FullName()
         {
