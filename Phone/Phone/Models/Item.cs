7ef43c9a1e591faca71eeed86735ef8dad065ed4
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-12-04 21:57:18 -0500

    updated gitignore

--- Phone/Phone/Models/Item.cs
+++ Phone/Phone/Models/Item.cs
@@ -7,6 +7,6 @@ namespace Phone.Models
         public string Id { get; set; }
         public string Text { get; set; }
         public string Description { get; set; }
-     //test change test vvvxvbvc El Nelson KLK
+     //test change test vvvxvbvc El Nelson KLK another change
     }
 }
