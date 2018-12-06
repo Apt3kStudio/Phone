ddc8f580504882fb21a535f1efb847094dfd3f2f
Nelson Vasquez <nelsonvasquez03@gmail.com>
2018-12-05 22:08:50 -0500

    nelson 2 files

--- Phone/Phone.iOS/AppDelegate.cs
+++ Phone/Phone.iOS/AppDelegate.cs
@@ -26,6 +26,7 @@ namespace Phone.iOS
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
+            //HEREEEEEEEEEEEEEEEEEEEEEEEEEEEEE
         }
     }
 }
