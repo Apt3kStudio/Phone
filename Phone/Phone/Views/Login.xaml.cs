c6a48afcbd96996c71148b3d2c1ef43a30c3f72e
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-12-17 22:05:05 -0500

    homepage

--- Phone/Phone/Views/Login.xaml.cs
+++ Phone/Phone/Views/Login.xaml.cs
@@ -15,11 +15,25 @@ namespace Phone.Views
         public Login()
         {
             InitializeComponent();
+            //var authenticator = new OAuth2Authenticator(
+            //clientId,
+            //null,
+            //Constants.Scope,
+            //new Uri(Constants.AuthorizeUrl),
+            //new Uri(redirectUri),
+            //new Uri(Constants.AccessTokenUrl),
+            //null,
+            //true);
         }
         async void NavigateToRegistrationPage_Clicked(object sender, EventArgs e)
         {
-            await DisplayAlert("SignUp", "You will be redirected to the Registration form", "OK");
+           // await DisplayAlert("SignUp", "You will be redirected to the Registration form", "OK");
             await Navigation.PushModalAsync(new NavigationPage(new Registration()));
+        }
+        async void goToStartUpPage(object sender, EventArgs e)
+        {
+            // await DisplayAlert("SignUp", "You will be redirected to the Registration form", "OK");
+            await Navigation.PushModalAsync(new NavigationPage(new StartUpPage()));
         }
     }
 }
--- Phone/Phone/Views/Setting.xaml.cs
+++ Phone/Phone/Views/Setting.xaml.cs
@@ -10,9 +10,9 @@ using Xamarin.Forms.Xaml;
 namespace Phone.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class Setting : ContentPage
+    public partial class settingpage : ContentPage
     {
-        public Setting()
+        public settingpage()
         {
             InitializeComponent();
         }
