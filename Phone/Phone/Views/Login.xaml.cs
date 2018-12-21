9f96ee4d366b579407aea2ea9f9dd91b71170cbc
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-12-21 17:14:13 -0500

    Loginpage Content class implemented PortalApiService authentication call #38

--- Phone/Phone/Views/Login.xaml.cs
+++ Phone/Phone/Views/Login.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Phone.Services;
+using Phone.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +14,22 @@ namespace Phone.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
+        private WebPortalApiServices apiService = new WebPortalApiServices();
+       public LoginUserViewModel UserVM { set; get; }
+        public string ErrorMessage { get; set; }
         public Login()
         {
             InitializeComponent();
+
+
+            UserVM = new LoginUserViewModel() {
+                 Email = "",
+                 Password = ""
+            };
+
+
+            BindingContext = UserVM;
+
             //var authenticator = new OAuth2Authenticator(
             //clientId,
             //null,
@@ -32,8 +47,22 @@ namespace Phone.Views
         }
         async void goToStartUpPage(object sender, EventArgs e)
         {
+
+            if (!string.IsNullOrEmpty(UserVM.Email)&& !string.IsNullOrEmpty(UserVM.Password))
+            {
+                UserVM.SigningInCommand.Execute(null);
+                if (UserVM.isAuthenticated)
+                {
+                    await Navigation.PushModalAsync(new NavigationPage(new StartUpPage()));
+                }
+                await DisplayAlert("Login", UserVM.Message, "OK");
+            }
+
+         
+
             // await DisplayAlert("SignUp", "You will be redirected to the Registration form", "OK");
-            await Navigation.PushModalAsync(new NavigationPage(new StartUpPage()));
+
+
         }
     }
 }
