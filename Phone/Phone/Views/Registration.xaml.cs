383428d937cfc4cb04d502dd568931a7756c71cb
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-12-21 17:17:15 -0500

    Api Call #40

--- Phone/Phone/Views/Registration.xaml.cs
+++ Phone/Phone/Views/Registration.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Phone.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,22 @@ namespace Phone.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Registration : ContentPage
     {
+        RegisterViewModel RegisterVM { get; set; }
+
         public Registration()
         {
             InitializeComponent();
+            RegisterVM = new RegisterViewModel()
+            {
+                Email = "Admin@Admin.com",
+                Password = "Password@132",
+                ConfirmPassword = "Password@132"
+            };
+        
+           // var s = RegisterVM.RegisterCommand.Execute(null);
+            BindingContext = RegisterVM;
         }
+
         async void BackToLogin(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new Login()));
