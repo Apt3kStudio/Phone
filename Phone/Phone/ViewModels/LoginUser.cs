a1e06a690b0a6a4fcf531a757efc868e1efdb476
DESKTOP-0LHOCST\Diosc <dioscarr@gmail.com>
2018-12-21 17:25:34 -0500

    New Login, Registration, Use Interface #38 #40

---
+++ Phone/Phone/ViewModels/LoginUser.cs
@@ -0,0 +1,46 @@
+﻿using Phone.Models;
+using Phone.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Phone.ViewModels
+{
+    public class LoginUserViewModel : IUser
+    {
+        private WebPortalApiServices _portalApiService = new WebPortalApiServices();
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public string Message { get; private set; }
+
+        public bool isAuthenticated { get; set; }
+        public Command SigningInCommand
+        {
+            get
+            {
+                return new Command(async () =>
+                {
+                    var isSuccess = await _portalApiService.SigningIn(Email, Password);
+                    if (isSuccess)
+                    {
+                        isAuthenticated = true;
+                        Message = "Registration Successfully!";
+                    }
+                    else
+                    {
+                        isAuthenticated = false;
+                        Message = "Try again.";
+                    }
+                });
+            }
+            set
+            {
+
+            }
+        }
+
+    }
+}
---
+++ Phone/Phone/ViewModels/RegisterViewModel.cs
@@ -0,0 +1,45 @@
+﻿using Phone.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Phone.ViewModels
+{
+   public class RegisterViewModel
+    {
+     private WebPortalApiServices _portalApiService = new WebPortalApiServices();
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ConfirmPassword { get; set; }
+
+
+        public string Message { get; set; }
+
+        public ICommand  RegisterCommand
+        {
+            get
+            {
+                return new Command(async() =>
+                {
+                    var isSuccess = await _portalApiService.RegisterAsync(Email, Password, ConfirmPassword);
+                    if (isSuccess)
+                    {
+                        Message = "Registration Successfully!";
+                    }
+                    else
+                    {
+                        Message = "Try again.";
+                    }
+                });
+            }
+            set
+            {
+
+            }
+        }
+
+    }
+}
