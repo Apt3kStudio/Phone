85996a6bc8e6a38b30f49f369d1488c9e31afc61
Nelson Vasquez <nelsonvasquez03@gmail.com>
2018-12-29 17:44:43 -0500

    #46 #48 Added vibration and flashing to the phone test page

---
+++ Phone/Phone/Models/EventModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Phone.Models
+{
+    public interface IEventModel
+    {
+        string EventID { get; set; }
+        string EventName { get; set; }
+        string EventMessage { get; set; }
+        string EventDuration { get; set; }
+    }
+}
---
+++ Phone/Phone/ViewModels/EventViewModel.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Essentials;
+using Phone.Models;
+using System.Threading.Tasks;
+namespace Phone.ViewModels
+{
+    public class EventViewModel : IEventModel
+    {
+        public string EventID { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string EventName { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string EventMessage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public string EventDuration { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string VibrateMe(int inDuration = 3)
+    {
+            string isVibrate ="";
+            try
+            {
+                
+                Vibration.Vibrate();
+                var duration = TimeSpan.FromSeconds(inDuration);
+                Vibration.Vibrate(duration);
+                isVibrate = "Success";
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+                isVibrate = "Not Supported";
+            }
+            catch (Exception ex)
+            {
+                isVibrate = "Error";
+            }
+            return isVibrate;
+
+        }
+        public void CancelVibration()
+        {
+            try
+            {
+                Vibration.Cancel();
+            }
+            catch (FeatureNotSupportedException ex)
+            {
+            }
+            catch (Exception ex)
+            {
+            }
+        }
+        public async Task FlashLighOnAsync()
+        {
+            try
+            {
+                // Turn On
+                await Flashlight.TurnOnAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+
+        public async Task FlashLighOffAsync()
+        {
+            try
+            { 
+                // Turn Off
+                await Flashlight.TurnOffAsync();
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+    }
+}
